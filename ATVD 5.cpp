8efd826da5a5789714e331d53daca2c28b340f02
Scruffy01 <160291778+Scruffy01@users.noreply.github.com>
2024-04-02 13:51:27 -0300

    Add files via upload
    5. Crie uma função que receba por parâmetro um vetor de números inteiros, o tamanho do vetor e os endereços de duas variáveis inteiras (que podemos chamar de min e max). Ao passar essas variáveis para a função seu programa deverá analisar qual é o maior e o menor elemento do vetor e depositar esses elementos nas variáveis do parâmetro.

---
+++ ATVD 5.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14
+
+//Questão 5
+
+void calcularAreaVolume(double raio, double *area, double *volume) {
+    *area = 4 * PI * pow(raio, 2);
+    *volume = (4.0 / 3.0) * PI * pow(raio, 3);
+}
+
+int main() {
+    double raio, area, volume;
+
+    printf("Digite o raio da esfera: ");
+    scanf("%lf", &raio);
+
+    calcularAreaVolume(raio, &area, &volume);
+
+    printf("\n\nArea da superficie: %.2f\n", area);
+    printf("\n\nVolume: %.2f\n", volume);
+
+    return 0;
+}
